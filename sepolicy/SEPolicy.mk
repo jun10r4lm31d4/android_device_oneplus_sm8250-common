e0991fbeb30eb7535314593830706595ed240fab
Juansheng <jaycopter@foxmail.com>
2023-07-19 12:47:55 +0800

    sm8250-common: Add sepolicy rules for pixel power HAL
    Refs: RisingOSS project

---
+++ sepolicy/SEPolicy.mk
@@ -0,0 +1,5 @@
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+BOARD_VENDOR_SEPOLICY_DIRS += device/oneplus/sm8250-common/sepolicy/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/oneplus/sm8250-common/sepolicy/public
